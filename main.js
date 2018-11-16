c990dd6b6b23b06a1b4f42fe06d9900fa73e449c
Daniel Steven Rojas Munoz <99030617122@academico.icesi.edu.co>
2018-11-16 08:59:21 -0500

    el segundo commit ya ver las paginadsadsads

--- main.js
+++ main.js
@@ -16,6 +16,22 @@ app.get('/', (req, res) => {
     res.render('paginaB');
 })
 
+app.get('/paginaA', (req, res) => {
+
+    res.render('paginaA');
+})
+
+app.get('/paginaB', (req, res) => {
+
+    res.render('paginaB');
+})
+
+app.get('/paginaC', (req, res) => {
+
+    res.render('paginaC');
+})
+
+
 
 app.listen(1234, () => {
     console.log("Escuchando en el puerto 1234")
